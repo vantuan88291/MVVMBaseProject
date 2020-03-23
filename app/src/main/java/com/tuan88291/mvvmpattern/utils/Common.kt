74a391152f9dca01ae32907bd426140d13b61af6
vantuan88291 <vantuan88291@gmail.com>
2020-03-23 16:22:26 +0700

    init

--- app/src/main/java/com/tuan88291/mvvmpattern/utils/Common.kt
+++ app/src/main/java/com/tuan88291/mvvmpattern/utils/Common.kt
@@ -1,6 +1,6 @@
 package com.tuan88291.mvvmpattern.utils
 
 object Common {
-    val DOMAIN = "https://reqres.in/api/"
+    val DOMAIN = "https://reqres.in/apis/"
     val SHARED_PREFERENCE_NAME = "AppName"
 }
