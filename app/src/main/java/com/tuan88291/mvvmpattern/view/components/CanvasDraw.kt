e774982dda9a1c35f26728ad08938d4ce36df669
Tuan <vantuan88291@gmail.com>
2020-05-17 10:04:06 +0700

    Add clock

--- app/src/main/java/com/tuan88291/mvvmpattern/view/components/CanvasDraw.kt
+++ app/src/main/java/com/tuan88291/mvvmpattern/view/components/CanvasDraw.kt
@@ -5,34 +5,62 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 
 class CanvasDraw(context: Context, attrs: AttributeSet): View(context, attrs) {
     private var mPaint: Paint? = null
+    private var mPaint1: Paint? = null
+    private var mPaint2: Paint? = null
+    private var mText: TextPaint? = null
     init {
         mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
         mPaint?.setColor(Color.RED)
         mPaint?.style = Paint.Style.STROKE
-        mPaint?.strokeWidth = 10f
+        mPaint?.strokeWidth = 2f
+
+        mPaint1 = Paint(Paint.ANTI_ALIAS_FLAG)
+        mPaint1?.setColor(Color.BLUE)
+        mPaint1?.style = Paint.Style.STROKE
+        mPaint1?.strokeWidth = 2f
+
+        mText = TextPaint(Paint.ANTI_ALIAS_FLAG)
+        mText?.color = Color.RED
+        mText?.textSize = 45f
+        mText?.textAlign = Paint.Align.CENTER
+
+        mPaint2 = Paint(Paint.ANTI_ALIAS_FLAG)
+        mPaint2?.setColor(Color.RED)
+        mPaint2?.style = Paint.Style.STROKE
+        mPaint2?.strokeWidth = 30f
     }
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val arcSize = 500
+        val arcSize = (((width / 2.2) * 2) - 30).toFloat()
         val left = ((width / 2) - (arcSize / 2)).toFloat()
         val top = ((height / 2) - (arcSize / 2)).toFloat()
         val bottom = top + arcSize
         val right = left + arcSize
 
-        canvas?.drawArc(RectF(left, top, right, bottom), 30f, 300f, true, mPaint!!)
-        canvas?.drawCircle((left + (arcSize / 1.5)).toFloat(),
-            (top + (arcSize / 4)).toFloat(), 30f, mPaint!!)
+        canvas?.drawArc(RectF(left, top, right, bottom), 30f, 300f, false, mPaint2!!)
+        canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 2.2).toFloat(), mPaint!!)
+        canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), ((width / 2.2) - 30).toFloat(), mPaint1!!)
 
-        canvas?.drawCircle((left + (arcSize / 1.4)).toFloat(),
-            (top + (arcSize / 2)).toFloat(), 30f, mPaint!!)
+        canvas?.drawText("12", ((width / 2)).toFloat(),
+            (((height - (((width / 2.2) - 30) * 2)) / 2) + 45).toFloat(), mText!!)
 
-        canvas?.drawCircle((left + (arcSize)).toFloat(),
-            (top + (arcSize / 2)).toFloat(), 30f, mPaint!!)
+        val dis1 = (width - (((width / 2.2) - 30) * 2)) / 2
+        canvas?.drawText("09", (dis1 + 30).toFloat(),
+            (height / 2).toFloat(), mText!!)
+
+        val dis = (width - (((width / 2.2) - 30) * 2)) / 2
+        canvas?.drawText("15", (width - (dis + 35)).toFloat(),
+            (height / 2).toFloat(), mText!!)
+
+        val disy = (((height - (((width / 2.2) - 30) * 2))) / 2) + (((width / 2.2) - 30) * 2)
+        canvas?.drawText("18", ((width / 2)).toFloat(),
+            (disy - 10).toFloat(), mText!!)
 
     }
 }
