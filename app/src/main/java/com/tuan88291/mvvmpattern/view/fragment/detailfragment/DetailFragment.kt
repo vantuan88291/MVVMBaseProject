3c1ecfd0783711ce66a379ec07f2f214a129d970
vantuan88291 <vantuan88291@gmail.com>
2023-04-12 14:24:57 +0700

    Add example about save profile

--- app/src/main/java/com/tuan88291/mvvmpattern/view/fragment/detailfragment/DetailFragment.kt
+++ app/src/main/java/com/tuan88291/mvvmpattern/view/fragment/detailfragment/DetailFragment.kt
@@ -7,11 +7,14 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.tuan88291.mvvmpattern.BaseChildFragment
 import com.tuan88291.mvvmpattern.R
+import com.tuan88291.mvvmpattern.data.local.model.DataProfile
 import com.tuan88291.mvvmpattern.databinding.DetailFragmentBinding
 import com.tuan88291.mvvmpattern.view.fragment.profilefragment.ProfileFragment
+import org.koin.android.ext.android.inject
 
 class DetailFragment : BaseChildFragment() {
     private var binding: DetailFragmentBinding? = null
+    private val profile: DataProfile by inject()
 
     override fun setView(
         inflater: LayoutInflater,
@@ -23,6 +26,9 @@ class DetailFragment : BaseChildFragment() {
     }
 
     override fun viewCreated(view: View, savedInstanceState: Bundle?) {
+        profile.getName().observe(this, {
+            binding?.text?.text = it
+        })
         binding?.button2?.setOnClickListener {
             this.navigate(ProfileFragment())
         }
--- app/src/main/java/com/tuan88291/mvvmpattern/view/fragment/homefragment/HomeFragment.kt
+++ app/src/main/java/com/tuan88291/mvvmpattern/view/fragment/homefragment/HomeFragment.kt
@@ -13,17 +13,20 @@ import com.tuan88291.mvvmpattern.BaseFragment
 import com.tuan88291.mvvmpattern.R
 import com.tuan88291.mvvmpattern.State
 import com.tuan88291.mvvmpattern.data.local.entity.DataRoom
+import com.tuan88291.mvvmpattern.data.local.model.DataProfile
 import com.tuan88291.mvvmpattern.data.local.model.DataUser
 import com.tuan88291.mvvmpattern.databinding.HomeFragmentBinding
 import com.tuan88291.mvvmpattern.utils.SharedPrefs
 import com.tuan88291.mvvmpattern.view.fragment.detailfragment.DetailFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : BaseFragment() {
     private val homeViewModel: HomeViewModel by viewModel()
     private var binding: HomeFragmentBinding? = null
+    private val profile: DataProfile by inject()
     override fun setView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -45,6 +48,7 @@ class HomeFragment : BaseFragment() {
         binding?.apply {
             btn.setOnClickListener {
                 homeViewModel.insertDatabase(DataRoom("tuan", (0..10).random()))
+                profile.setData("tuan123", "abc")
             }
             button3.setOnClickListener {
                 navigate(DetailFragment())
